0b0cd843d14d8b0b94fbc33b11773a390dd36968
Tom E Flaherty <teflaherty@utexas.edu>
2025-06-14 20:44:52 -0500

    Add project files.

---
+++ StripIt/cmdStripIt.cs
@@ -0,0 +1,35 @@
+﻿namespace StripIt
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdStripIt : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // Revit application and document variables
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            // Your code goes here
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+
+            Common.ButtonDataClass myButtonData = new Common.ButtonDataClass(
+                buttonInternalName,
+                buttonTitle,
+                MethodBase.GetCurrentMethod().DeclaringType?.FullName,
+                Properties.Resources.Blue_32,
+                Properties.Resources.Blue_16,
+                "This is a tooltip for Button 1");
+
+            return myButtonData.Data;
+        }
+    }
+
+}
